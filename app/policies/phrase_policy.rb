77323cf2670c33c5a894693252eeb2f28abbb6bb
dutavi <tangvinhduong99@gmail.com>
2019-08-08 14:54:11 +0700

    Pundit user for phrase

--- app/policies/phrase_policy.rb
+++ app/policies/phrase_policy.rb
@@ -5,11 +5,24 @@ class PhrasePolicy < ApplicationPolicy
     super or current_user == model
   end
 
-  def destroy?
-    return false if current_user == model
-    super
+  
+  def edit?
+    update?
+  end
+  
+  def update?
+    user = current_user
+    phrase = model
+    return true if user.id == phrase.user_id
   end
 
+  def destroy?
+    user = current_user
+    phrase = model
+    return true if user.id == phrase.user_id
+    super
+  end
+  
   class Scope < Scope
     def resolve
       scope.where(id: current_user.id)
